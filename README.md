dc6ef8a19710ae9ce319823e027c184f6effc9ae
NitiMittal <mittalneetu15@gmail.com>
2022-02-03 20:07:37 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,59 @@
+# Frontend Mentor - Tip calculator app solution
+
+This is a solution to the [Tip calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/tip-calculator-app-ugJNGbJUX). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Overview
+
+### The challenge
+
+Your challenge is to build out this tip calculator app and get it looking as close to the design as possible.
+
+You can use any tools you like to help you complete the challenge. So if you've got something you'd like to practice, feel free to give it a go.
+
+Your users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Calculate the correct tip and total cost of the bill per person
+
+Want some support on the challenge? [Join our Slack community](https://www.frontendmentor.io/slack) and ask questions in the **#help** channel.
+
+### Screenshot
+
+.images/screenshot.jpg
+
+### Links
+
+- Solution URL: https://github.com/NitiMittal/Calculator-app-frontend.io
+- Live Site URL:
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+- Javascript
+
+### What I learned
+
+1. Use of validations.
+2. Positions in CSS
+3. How to call functions and use of radio buttons.
+
+### Useful resources
+
+1. https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_toggle_hide_show
+2. https://stackoverflow.com/questions/43785821/html5-validation-check-input-bigger-than-0
+
+## Author
+
+- Frontend Mentor - [@NitiMittal](https://www.frontendmentor.io/profile/NitiMittal)
+
+## Acknowledgments
+
+I took help from someone's else solution.
+https://www.frontendmentor.io/solutions/tip-calculator-5dor20aLM
---
+++ js/script.js
@@ -0,0 +1,78 @@
+let rupee = document.getElementById("rupee");
+let person = document.getElementById("person");
+let custom = document.getElementById("custom");
+let tip_amount = document.getElementById("tip_amount");
+let total_amount = document.getElementById("total_amount");
+let reset = document.getElementById("reset");
+let error = document.getElementById("error");
+let error1 = document.getElementById("error1");
+
+let bill_val,
+  people_val,
+  tip_value,
+  amt = 0,
+  ppl = 0;
+
+rupee.addEventListener("input", () => {
+  amt = Number(rupee.value);
+  if (amt <= 0 || amt == "") {
+    error.style.display = "block";
+  } else {
+    error.style.display = "none";
+    calculate();
+  }
+});
+
+person.addEventListener("input", () => {
+  ppl = Number(person.value);
+  if (ppl <= 0 || ppl == "") {
+    error1.style.display = "block";
+  } else {
+    error1.style.display = "none";
+    calculate();
+  }
+});
+
+custom.addEventListener("input", () => {
+  if (custom.value >= 0) {
+    clearRadioButtons();
+    tip_value = custom.value;
+    calculate();
+  }
+});
+
+// get value of radio button
+document.mainForm.onclick = () => {
+  tip_value = document.mainForm.radiotip.value;
+};
+
+const calculate = () => {
+  if (amt > 0 && ppl > 0) {
+    let total_tip = (amt * Number(tip_value)) / 100;
+    let total_amount1 = total_tip + amt;
+    let tip_per_person = total_tip / ppl;
+    let amount_per_person = total_amount1 / ppl;
+    tip_amount.innerHTML = "$" + tip_per_person.toFixed(2);
+    total_amount.innerHTML = "$" + amount_per_person.toFixed(2);
+  }
+};
+
+//reset all the values
+reset.addEventListener("click", () => {
+  tip_amount.innerHTML = "$0.00";
+  total_amount.innerHTML = "$0.00";
+  rupee.value = "";
+  person.value = "";
+  custom.value = "";
+  error.style.display = "none";
+  error1.style.display = "none";
+  clearRadioButtons();
+});
+
+const clearRadioButtons = () => {
+  let radioButtonArray = document.getElementsByName("radiotip");
+  for (let i = 0; i < radioButtonArray.length; i++) {
+    let radioButton = radioButtonArray[i];
+    radioButton.checked = false;
+  }
+};
